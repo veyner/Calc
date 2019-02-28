a97c93c54fe918e05f8c4634e8c7e0d3f67cb685
veyner <myfun239@mail.ru>
2019-02-28 19:59:35 +0300

    v0.2

---
+++ Calc/Parser.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Calc
+{
+    public enum Mode { RAD, DEG, GRAD };
+
+    public class Parser
+    {
+        private string Value;
+
+        public string Evaluate(string expression)
+        {
+            try
+            {
+                // Expression = "-(5 - 10)^(-1)  ( 3 + 2(    cos( 3 Pi )+( 2+ ln( exp(1) ) )    ^3))"
+                // Убираются пробелы
+                // -(5 - 10)^(-1)  ( 3 + 2(    cos( 3 Pi )+( 2+ ln( exp(1) ) )    ^3)) -> -(5-10)^(-1)(3+2(cos(3Pi)+(2+ln(exp(1)))^3))
+                expression = expression.Replace(" ", "");
+                // Добавление знака умножения, где он необходим для корректных вычислений
+                //                                                             _    _      _
+                // -(5-10)^(-1)(3+2(cos(3Pi)+(2+ln(exp(1)))^3)) -> -(5-10)^(-1)*(3+2*(cos(3*Pi)+(2+ln(exp(1)))^3))
+                //             |   |     |
+                /* Расшифровка шаблона Regex
+                 * (?<=[\d\)]) если предшествует одна из цифр, закрывающая скобка
+                 * (?=[a-df-z\(]) если последует одна из букв, открывающая скобка
+                 * | или
+                 * (?<=pi) если предшествует pi
+                 * (?=[^\+\-\*\/\\^!)]) если последует ни один из символов
+                 * | или
+                 * (?<=\)) если  предшествует закрывающая скобка
+                 * (?=\d) если последует одна из цифр
+                 * | или
+                 * (?<=[^\/\*\+\-]) если предшествует ни один из символов
+                 * (?=exp) если последует exp
+                 */
+                Regex regEx = new Regex(@"(?<=[\d\)])(?=[a-df-z\(])|(?<=pi)(?=[^\+\-\*\/\\^!)])|(?<=\))(?=\d)|(?<=[^\/\*\+\-\(])(?=exp)", RegexOptions.IgnoreCase);
+                expression = regEx.Replace(expression, "*");
+                //Замена pi на 3,14
+                //                                                                        ____
+                // -(5-10)^(-1)*(3+2*(cos(3*Pi)+(2+ln(e))^3)) -> -(5-10)^(-1)*(3+2*(cos(3*3,14)+(2+ln(exp(1)))^3))
+                //                          --
+                regEx = new Regex("pi", RegexOptions.IgnoreCase);
+                expression = regEx.Replace(expression, Math.PI.ToString());
+                // Поиск и вычисление выражений в скобках
+                //                                                       _____
+                // -(5-10)^(-1)*(3+2*(cos(3*3,14)+(2+ln(exp(1)))^3)) -> -{-5}^(-1)*(3+2*(cos(3*3,14)+(2+ln(exp(1)))^3))
+                //  |_____|
+                //                                                          __
+                // -{-5}^(-1)*(3+2*(cos(3*3,14)+(2+ln(exp(1)))^3)) -> -{-5}^-1*(3+2*(cos(3*3,14)+(2+ln(exp(1)))^3))
+                //       |__|
+                //                                                                    ____
+                // -{-5}^-1*(3+2*(cos(3*3,14)+(2+ln(exp(1)))^3)) -> -{-5}^-1*(3+2*(cos9,42+(2+ln(exp(1)))^3))
+                //                   |______|
+                //                                                                              _
+                // -{-5}^-1*(3+2*(cos9,72+(2+ln(exp(1)))^3)) -> -{-5}^-1*(3+2*(cos9,72+(2+ln(exp1))^3))
+                //                                 |_|
+                //                                                                        ____
+                // -{-5}^-1*(3+2*(cos9,72+(2+ln(exp1))^3)) -> -{-5}^-1*(3+2*(cos9,72+(2+ln2,71)^3))
+                //                             |____|
+                //                                                                 ____
+                // -{-5}^-1*(3+2*(cos9,72+(2+ln2,71)^3)) -> -{-5}^-1*(3+2*(cos9,72+{3}^3))
+                //                        |_________|
+                //                                                 __
+                // -{-5}^-1*(3+2*(cos9,72+{3}^3)) -> -{-5}^-1*(3+2*26)
+                //               |_____________|
+                //                               __
+                // -{-5}^-1*(3+2*26) -> -{-5}^-1*55
+                //          |______|
+                /*Расшифровка шаблона Regex
+                 *([a-z]*) - одна из букв
+                 * и
+                 * \( открывающая скобка
+                 * (([^\(\)]+)\) - не скобки
+                 * \) закрывающая скобка
+                 * и
+                 * (\^|!?) - возведение в степень или факториал
+                 */
+                regEx = new Regex(@"([a-z]*)\(([^\(\)]+)\)(\^|!?)", RegexOptions.IgnoreCase);
+                Match m = regEx.Match(expression);
+                while (m.Success)
+                {
+                    if (m.Groups[3].Value.Length > 0)
+                    {
+                        expression = expression.Replace(m.Value, "{" + m.Groups[1].Value + new Solver().Solve(m.Groups[2].Value) + "}" + m.Groups[3].Value);
+                    }
+                    else expression = expression.Replace(m.Value, m.Groups[1].Value + new Solver().Solve(m.Groups[2].Value));
+                    m = regEx.Match(expression);
+                }
+                // Если нет скобок, вычисление выражения
+                //                __
+                // -{-5}^-1*55 => 11
+                // |_________|
+                //
+                Value = new Solver().Solve(expression);
+                return Value;
+            }
+            catch
+            {
+                // Shit!
+                return "error";
+            }
+        }
+    }
+}
---
+++ Calc/Solver.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Calc
+{
+    public class Solver
+    {
+        private ArrayList FunctionList = new ArrayList(new string[] { "abs", "acos", "asin", "atan", "ceil", "cos", "cosh", "exp", "floor", "ln", "log", "sign", "sin", "sinh", "sqrt", "tan", "tanh" });
+        private double Factor;
+        private Mode mode;
+
+        // Constructor
+        public Solver()
+        {
+            Mode = Mode.RAD;
+        }
+
+        public Solver(Mode mode)
+        {
+            Mode = mode;
+        }
+
+        public Mode Mode
+        {
+            get { return this.mode; }
+            set
+            {
+                this.mode = value;
+                switch (value)
+                {
+                    case Mode.RAD:
+                        this.Factor = 1.0;
+                        break;
+
+                    case Mode.DEG:
+                        this.Factor = 2.0 * Math.PI / 360.0;
+                        break;
+
+                    case Mode.GRAD:
+                        this.Factor = 2.0 * Math.PI / 400.0;
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Решение функций из листа FuctionList
+        /// </summary>
+        /// <param name="expression">Выражение</param>
+        /// <returns>Выражение с вычисленными функциями из листа FunctionList</returns>
+        private string SolveAbc(string expression)
+        {
+            /* Расшифровка шаблона Regex
+             * ([a-z]{2,}) - 2 и более букв подряд
+             * ([\+-]?\d+,*\d*[eE][\+-]*\d+|[\+-]?\d+,*\d*) - один из знаков + -, цифры, запятая, цифры, e или E(*10^X),один из знаков + -, цифры
+             */
+            Regex regEx = new Regex(@"([a-z]{2,})([\+-]?\d+,*\d*[eE][\+-]*\d+|[\+-]?\d+,*\d*)", RegexOptions.IgnoreCase);
+            Match m = regEx.Match(expression);
+            while (m.Success && FunctionList.IndexOf(m.Groups[1].Value.ToLower()) > -1)
+            {
+                switch (m.Groups[1].Value.ToLower())
+                {
+                    case "abs":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Abs(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "acos":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Acos(Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "asin":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Asin(Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "atan":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Atan(Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "cos":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Cos(Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "ceil":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Ceiling(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "cosh":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Cosh(Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "exp":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Exp(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "floor":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Floor(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "ln":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Log(Convert.ToDouble(m.Groups[2].Value), Math.Exp(1.0)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "log":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Log10(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "sign":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Sign(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "sin":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Sin(this.Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "sinh":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Sinh(this.Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "sqrt":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Sqrt(Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "tan":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Tan(this.Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                    case "tanh":
+                        expression = expression.Replace(m.Groups[0].Value, Math.Tanh(this.Factor * Convert.ToDouble(m.Groups[2].Value)).ToString());
+                        m = regEx.Match(expression);
+                        continue;
+                }
+            }
+            return expression;
+        }
+
+        /// <summary>
+        /// Нахождение факториала
+        /// </summary>
+        /// <param name="expression">Выражение</param>
+        /// <returns>Выражение с вычисленным факториалом</returns>
+        private string SolveFactorial(string expression)
+        {
+            // Шаблон Regex для {x}!
+            Regex regEx = new Regex(@"\{(.+)\}!");
+            Match m = regEx.Match(expression);
+            while (m.Success)
+            {
+                double n = Convert.ToDouble(m.Groups[1].Value);
+                if ((n < 0) && (n != Math.Round(n)))
+                {
+                    throw new Exception(); // Если значение отрицательное или не целое - выдать ошибку
+                }
+                expression = regEx.Replace(expression, Fact(Convert.ToDouble(m.Groups[1].Value)).ToString(), 1);
+                m = regEx.Match(expression);
+            }
+            // Расшифровка шаблона Regex  - один из знаков + -, цифры, запятая, цифры, e или E(*10^X),один из знаков + -, цифры, знак факториала
+            regEx = new Regex(@"(\d+,*\d*[eE][\+-]?\d+|\d+,*\d*)!"); // Search for patterns like 5!
+            m = regEx.Match(expression);
+            while (m.Success)
+            {
+                double n = Convert.ToDouble(m.Groups[1].Value);
+                if ((n < 0) && (n != Math.Round(n)))
+                {
+                    throw new Exception(); // Value negative or not integer -> throw exception
+                }
+                expression = regEx.Replace(expression, Fact(Convert.ToDouble(m.Groups[1].Value)).ToString(), 1);
+                m = regEx.Match(expression);
+            }
+            return expression;
+        }
+
+        /// <summary>
+        /// Вычисление степени
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns>Выражение с числами возведенными в степень</returns>
+        public string SolvePower(string expression)
+        {
+            /* Расшифровка шаблона Regex для варианта {-x}^-y
+             * \{(.+)\}\ - цифры в фигурных скобках
+             * (-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*) - знак -, цифры, запятая, цифры,  e или E(*10^X),один из знаков + -, цифры или знак -, цифры, запятая, цифры
+             */
+            Regex regEx = new Regex(@"\{(.+)\}\^(-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*)");
+            Match m = regEx.Match(expression, 0);
+            while (m.Success)
+            {
+                expression = expression.Replace(m.Value, Math.Pow(Convert.ToDouble(m.Groups[1].Value), Convert.ToDouble(m.Groups[2].Value)).ToString());
+                m = regEx.Match(expression);
+            }
+            /* Расшифровка шаблона Regex для варианта x^-y
+             * (\d+,*\d*e[\+-]?\d+|\d+,*\d*) - цифры, запятая, цифры, е, один из знаков +-, цифры или цифры, запятая, цифры
+             * (-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*) - знак -, цифры, запятая, цифры, один из е Е, один из знаков + -, цифры или знак -,цифры,запятая,цифры
+             */
+            regEx = new Regex(@"(\d+,*\d*e[\+-]?\d+|\d+,*\d*)\^(-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*)");
+            m = regEx.Match(expression, 0);
+            while (m.Success)
+            {
+                expression = regEx.Replace(expression, Math.Pow(Convert.ToDouble(m.Groups[1].Value), Convert.ToDouble(m.Groups[2].Value)).ToString(), 1);
+                m = regEx.Match(expression);
+            }
+            return expression;
+        }
+
+        /// <summary>
+        /// Вычисление умножения и деления
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        private string SolveMultiDivis(string expression)
+        {
+            /* Расшифровка шаблона Regex
+             * ([\+-]?\d+,*\d*[eE][\+-]?\d+|[\-\+]?\d+,*\d*) - один из знаков + -, цифры, запятая, цифры, один из е Е, один из знаков + -, цифры или один из знаков + -,цифры,запятая,цифры
+             * ([\/\*]) - один из знаков * /
+             * (-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*) - знак -, цифры, запятая, цифры, один из е Е, один из знаков + -, цифры или знак -,цифры,запятая,цифры
+             */
+            Regex regEx = new Regex(@"([\+-]?\d+,*\d*[eE][\+-]?\d+|[\-\+]?\d+,*\d*)([\/\*])(-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*)");
+            Match m = regEx.Match(expression, 0);
+            while (m.Success)
+            {
+                double result;
+                switch (m.Groups[2].Value)
+                {
+                    case "*":
+                        result = Convert.ToDouble(m.Groups[1].Value) * Convert.ToDouble(m.Groups[3].Value);
+                        if ((result < 0) || (m.Index == 0))
+                        {
+                            expression = regEx.Replace(expression, result.ToString(), 1);
+                        }
+                        else
+                        {
+                            expression = expression.Replace(m.Value, "+" + result);
+                        }
+                        m = regEx.Match(expression);
+                        continue;
+                    case "/":
+                        result = Convert.ToDouble(m.Groups[1].Value) / Convert.ToDouble(m.Groups[3].Value);
+                        if ((result < 0) || (m.Index == 0))
+                        {
+                            expression = regEx.Replace(expression, result.ToString(), 1);
+                        }
+                        else
+                        {
+                            expression = regEx.Replace(expression, "+" + result, 1);
+                        }
+                        m = regEx.Match(expression);
+                        continue;
+                }
+            }
+            return expression;
+        }
+
+        /// <summary>
+        /// Вычисление суммы и разности
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        private string SolveAddSubtract(string expression)
+        {
+            /* Расшифровка шаблоа Regex
+             * ([\+-]?\d+,*\d*[eE][\+-]?\d+|[\-\+]?\d+,*\d*) - один из знаков + -, цифры, запятая, цифры, один из е Е, один из знаков + -, цифры или один из знаков + -,цифры,запятая,цифры
+             * ([\+-]) - один из знаков + -
+             * (-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*) - знак -, цифры, запятая, цифры, один из е Е, один из знаков + -, цифры или знак -,цифры,запятая,цифры
+             */
+            Regex regEx = new Regex(@"([\+-]?\d+,*\d*[eE][\+-]?\d+|[\+-]?\d+,*\d*)([\+-])(-?\d+,*\d*[eE][\+-]?\d+|-?\d+,*\d*)");
+            Match m = regEx.Match(expression, 0);
+            while (m.Success)
+            {
+                double result;
+                switch (m.Groups[2].Value)
+                {
+                    case "+":
+                        result = Convert.ToDouble(m.Groups[1].Value) + Convert.ToDouble(m.Groups[3].Value);
+                        if ((result < 0) || (m.Index == 0))
+                        {
+                            expression = regEx.Replace(expression, result.ToString(), 1);
+                        }
+                        else
+                        {
+                            expression = regEx.Replace(expression, "+" + result, 1);
+                        }
+                        m = regEx.Match(expression);
+                        continue;
+                    case "-":
+                        result = Convert.ToDouble(m.Groups[1].Value) - Convert.ToDouble(m.Groups[3].Value);
+                        if ((result < 0) || (m.Index == 0))
+                        {
+                            expression = regEx.Replace(expression, result.ToString(), 1);
+                        }
+                        else
+                        {
+                            expression = regEx.Replace(expression, "+" + result, 1);
+                        }
+                        m = regEx.Match(expression);
+                        continue;
+                }
+            }
+            return expression;
+        }
+
+        public string Solve(string expression)
+        {
+            expression = SolveAbc(expression);
+            expression = SolveFactorial(expression);
+            expression = SolvePower(expression);
+            expression = SolveMultiDivis(expression);
+            expression = SolveAddSubtract(expression);
+
+            if (expression.StartsWith("--")) expression = expression.Substring(2);
+            return expression;
+        }
+
+        // Calculate Factorial.
+        private double Fact(double n)
+        {
+            return n == 0.0 ? 1.0 : n * Fact(n - 1.0);
+        }
+    }
+}
